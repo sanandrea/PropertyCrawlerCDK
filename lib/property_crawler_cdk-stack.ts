4f58cbb827b7de6645577af0fe52d2f41846566e
Andi Palo <sanandrea8080@gmail.com>
2022-10-22 23:25:20 +0100

    deploy lambda assets and 1 lambda

--- lib/property_crawler_cdk-stack.ts
+++ lib/property_crawler_cdk-stack.ts
@@ -1,19 +1,28 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Function, Code, Runtime } from "aws-cdk-lib/aws-lambda";
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import * as assets from "aws-cdk-lib/aws-s3-assets";
+import { resolve } from 'path';
 
 export class PropertyCrawlerCdkStack extends Stack {
+  public lambdaAsset: assets.Asset;
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'PropertyCrawlerCdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    this.lambdaAsset = new assets.Asset(this, "LambdaAssetsZip", {
+      path: resolve(__dirname, "../../PropertyCrawlerLambda/dist/property-crawler-0.0.1.zip"),
     });
 
-    const topic = new sns.Topic(this, 'PropertyCrawlerCdkTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const crawler = new Function(this, "CrawlerFunction", {
+      runtime: Runtime.PYTHON_3_8,
+      code: Code.fromBucket(
+        this.lambdaAsset.bucket,
+        this.lambdaAsset.s3ObjectKey
+        ),
+      handler: 'lambda_handlers.crawler.lambda_handler'
+    });
   }
 }
