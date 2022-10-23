7004c018ef00d39b6defc841db6bc9a6e2b18148
Andi Palo <sanandrea8080@gmail.com>
2022-10-23 22:53:20 +0100

    added more stack components

--- lib/property_crawler_cdk-stack.ts
+++ lib/property_crawler_cdk-stack.ts
@@ -1,11 +1,14 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Function, Code, Runtime } from "aws-cdk-lib/aws-lambda";
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb"
+import * as iam from 'aws-cdk-lib/aws-iam';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
 import * as assets from "aws-cdk-lib/aws-s3-assets";
 import { resolve } from 'path';
+import { Rule, Schedule } from 'aws-cdk-lib/aws-events';
+import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
+
 
 export class PropertyCrawlerCdkStack extends Stack {
   public lambdaAsset: assets.Asset;
@@ -16,13 +19,52 @@ export class PropertyCrawlerCdkStack extends Stack {
       path: resolve(__dirname, "../../PropertyCrawlerLambda/dist/property-crawler-0.0.1.zip"),
     });
 
+    const lambdaRole = new iam.Role(this, 'Role', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      description: 'Role for the Crawler Lambda',
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSQSFullAccess'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBFullAccess')
+      ]
+    });
+    const table = new dynamodb.Table(this, 'Table', {
+      tableName: 'PropertiesList',
+      partitionKey: { name: 'pr_id', type: dynamodb.AttributeType.STRING },
+      sortKey: {name: 'sk', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      encryption: dynamodb.TableEncryption.AWS_MANAGED // Customer managed costs 1$ per month.
+    });
+
     const crawler = new Function(this, "CrawlerFunction", {
       runtime: Runtime.PYTHON_3_8,
+      functionName: 'MainPropertyCrawler',
+      memorySize: 1024,
+      timeout: Duration.minutes(3),
+      role: lambdaRole,
       code: Code.fromBucket(
         this.lambdaAsset.bucket,
         this.lambdaAsset.s3ObjectKey
         ),
-      handler: 'lambda_handlers.crawler.lambda_handler'
+      handler: 'lambda_handlers.crawler.lambda_handler',
+      environment: {
+        CRAWLER_TABLE_NAME: table.tableName
+      },
     });
+
+    const dlq = new sqs.Queue(this, 'Queue', {
+      queueName: 'CrawlerDLQ'
+    });
+
+    const lambdaTaskTarget = new LambdaFunction(crawler, {
+      deadLetterQueue:dlq,
+      retryAttempts: 2
+    });
+
+    new Rule(this, 'ScheduleRule', {
+      ruleName: 'PropertyCrawlerTrigger',
+      schedule: Schedule.cron({ minute: '0', hour: '12' }),
+      targets: [lambdaTaskTarget],
+     });
   }
 }
